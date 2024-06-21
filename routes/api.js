76b1dab48a3fd6c5be59e2f2a87d3ecaa0719e6a
Mwenya <mwen@hotmail.co.uk>
2024-06-21 14:59:29 +0100

    fix: updated routes to correctly out invalid number/unit

--- routes/api.js
+++ routes/api.js
@@ -6,38 +6,44 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
   app.get('/api/convert', (req, res) => {
+    const invalidNum = 'INVALID NUMBER';
+    const invalidUnit = 'INVALID UNIT';
 
     let convertHandler = new ConvertHandler();
 
     let string = req.query.input;
 
-    try {
-
       const getNum = convertHandler.getNum(string);
-
       const getUnit = convertHandler.getUnit(string);
 
-      const getReturnUnit = convertHandler.getReturnUnit(getUnit);
+      if (getNum === 'INVALID NUMBER' && getUnit === 'INVALID UNIT') {
 
-      const spellOutUnit = convertHandler.spellOutUnit(getUnit);
-      const spellOutInitUnit = convertHandler.spellOutUnit(getReturnUnit);
+         res.status(200).send('invalid number and unit')
+       
+      } else if (getNum === 'INVALID NUMBER') {
 
-      const convert = convertHandler.convert(getNum, getUnit);
+        return res.status(200).send('invalid number')
 
-      const getString = convertHandler.getString(getNum, spellOutUnit, convert, spellOutInitUnit);
+      } else if (getUnit === 'INVALID UNIT') {
 
-      if ( getNum === 'Invalid Input' || getUnit === 'INVALID UNIT') {
-        
         return res.status(200).send('invalid unit')
+
       } else {
-        
-        res.status(200).json({ initNum: getNum, initUnit: getUnit, returnNum: convert, returnUnit: getReturnUnit, string: getString });
-        
+        const getReturnUnit = convertHandler.getReturnUnit(getUnit);
+        const spellOutUnit = convertHandler.spellOutUnit(getUnit);
+        const spellOutInitUnit = convertHandler.spellOutUnit(getReturnUnit);
+        const convert = convertHandler.convert(getNum, getUnit);
+        const getString = convertHandler.getString(getNum, spellOutUnit, convert, spellOutInitUnit);
+
+       return res.status(200).json({
+          initNum: getNum,
+          initUnit: getUnit,
+          returnNum: convert,
+          returnUnit: getReturnUnit,
+          string: getString
+        });
+
       }
-    }
-    catch (error) {
-      res.status(200).send('invalid unit')
-    }
 
   })
 
